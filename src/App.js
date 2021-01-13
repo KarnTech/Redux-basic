f36e71c9520c73e6b93a049617d5b86a7166c32c
Karn Singh <gurkaran.12@gmail.com>
2021-01-12 20:06:56 -0500

    reducer implemented?

--- src/App.js
+++ src/App.js
@@ -7,21 +7,37 @@ import cartItems from "./cart-items";
 // redux stuff
 import {createStore}  from 'redux'
 //initial store 
+
+
+import * as actions from './actions'
 const initialstate  = {
-  count: 78
+  count: 0, 
+  name: 'john'
 }
 function reducer(state, action){
   console.log({state, action})
   if(action.type ==="decrease"){
-    return {count: state.count-1}
+    return {...state, count: state.count-1 }
   }
+
+  if(action.type ==="increase"){
+    return {...state, count: state.count+1 }
+  }
+
+
   return state
 }
 
 const store  =  createStore(reducer, initialstate)
 console.log(store)
 
-store.dispatch({type:'decrease'})
+store.dispatch({type: actions.increase})
+
+store.dispatch({type:actions.decrease})
+
+
+
+
 console.log(store.getState())
 function App() {
   // cart setup
---
+++ src/actions.js
@@ -0,0 +1,8 @@
+export const increase  = "increase"
+export const decrease  = "decrease"
+export const remove  = "remove"
+export const clearcart = "clearcart"
+export const gettotal =  "gettotal"
+export const getamount = "getamount"
+
+
